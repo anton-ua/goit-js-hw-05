022709dfedc51472a5746e71132f3d74049bbf0e
anton-ua <antonyua23@gmail.com>
2019-12-08 15:50:43 +0200

    hw

---
+++ task-03.js
@@ -0,0 +1,39 @@
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(item) {
+    this.items.push(item);
+    return this.items;
+  }
+
+  removeItem(item) {
+    for (let elem in this.items) {
+      if (this.items[elem] === item) {
+        this.items.splice(elem, 1);
+      }
+    }
+    return this.items;
+  }
+}
+
+const storage = new Storage([
+  "Нанитоиды",
+  "Пролонгер",
+  "Железные жупи",
+  "Антигравитатор"
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem("Дроид");
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem("Пролонгер");
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
